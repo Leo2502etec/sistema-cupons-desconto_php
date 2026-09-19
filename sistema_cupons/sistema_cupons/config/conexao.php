216293edbdbf36d606fdd2d16d9521a117205afe
Leonardo Gregorio de Sousa <leo.gregorio2010@gmail.com>
2026-09-19 02:41:56 -0300

    Corrige arquivos do sistema

---
+++ sistema_cupons/sistema_cupons/config/conexao.php
@@ -0,0 +1,22 @@
+<?php
+
+$host = "localhost";
+$banco = "sistema_cupons";
+$usuario = "root";
+$senha = "";
+
+try {
+    $conexao = new PDO(
+        "mysql:host=$host;dbname=$banco;charset=utf8mb4",
+        $usuario,
+        $senha
+    );
+
+    $conexao->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch (PDOException $erro) {
+    die("Erro na conexão: " . $erro->getMessage());
+}
